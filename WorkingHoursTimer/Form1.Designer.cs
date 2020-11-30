1a2c4707e11eeec54ee29f9877abeb22f9091159
Alexander Seabrook <alexander.s@bewiser.co.uk>
2020-11-30 15:18:38 +0000

    Initial Build

--- WorkingHoursTimer/Form1.Designer.cs
+++ WorkingHoursTimer/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StartStop = new System.Windows.Forms.Button();
+            this._totalElapsedTimeDisplay = new System.Windows.Forms.Label();
+            this._currentElapsedTimeDisplay = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartStop
+            // 
+            this.StartStop.Location = new System.Drawing.Point(12, 109);
+            this.StartStop.Name = "StartStop";
+            this.StartStop.Size = new System.Drawing.Size(238, 44);
+            this.StartStop.TabIndex = 0;
+            this.StartStop.Text = "Start";
+            this.StartStop.UseVisualStyleBackColor = true;
+            this.StartStop.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // _totalElapsedTimeDisplay
+            // 
+            this._totalElapsedTimeDisplay.AutoSize = true;
+            this._totalElapsedTimeDisplay.Location = new System.Drawing.Point(109, 28);
+            this._totalElapsedTimeDisplay.Name = "_totalElapsedTimeDisplay";
+            this._totalElapsedTimeDisplay.Size = new System.Drawing.Size(0, 13);
+            this._totalElapsedTimeDisplay.TabIndex = 2;
+            // 
+            // _currentElapsedTimeDisplay
+            // 
+            this._currentElapsedTimeDisplay.AutoSize = true;
+            this._currentElapsedTimeDisplay.Location = new System.Drawing.Point(109, 60);
+            this._currentElapsedTimeDisplay.Name = "_currentElapsedTimeDisplay";
+            this._currentElapsedTimeDisplay.Size = new System.Drawing.Size(0, 13);
+            this._currentElapsedTimeDisplay.TabIndex = 3;
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(85, 159);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(94, 31);
+            this.Submit.TabIndex = 4;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Total Time Elapsed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Current Shift Time Elapsed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(262, 245);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this._currentElapsedTimeDisplay);
+            this.Controls.Add(this._totalElapsedTimeDisplay);
+            this.Controls.Add(this.StartStop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TimeSheet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button StartStop;
+        private System.Windows.Forms.Label _totalElapsedTimeDisplay;
+        private System.Windows.Forms.Label _currentElapsedTimeDisplay;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WorkingHoursTimer/Form1.cs
+++ WorkingHoursTimer/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,98 @@ namespace WorkingHoursTimer
         public Form1()
         {
             InitializeComponent();
+            _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += new EventHandler(_timer_Tick);
         }
+
+        private Timer _timer;
+        private string _Start = null;
+        private DateTime _startTime = DateTime.MinValue;
+        private TimeSpan _currentElapsedTime = TimeSpan.Zero;
+        private TimeSpan _totalElapsedTime = TimeSpan.Zero;
+        private bool _timerRunning = false;
+       
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            var timeSinceStartTime = DateTime.Now - _startTime;
+            timeSinceStartTime = new TimeSpan(timeSinceStartTime.Hours,
+                                              timeSinceStartTime.Minutes,
+                                              timeSinceStartTime.Seconds);
+            _currentElapsedTime = timeSinceStartTime + _totalElapsedTime;
+            _totalElapsedTimeDisplay.Text = _currentElapsedTime.ToString();
+            _currentElapsedTimeDisplay.Text = timeSinceStartTime.ToString();
+        }
+
+       
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if(_Start == null)
+                _Start = DateTime.Now.ToString();
+            if (!_timerRunning)
+            {
+                _startTime = DateTime.Now;
+                _totalElapsedTime = _currentElapsedTime;
+                _timer.Start();
+                _timerRunning = true;
+                this.StartStop.Text = "Pause";
+            }
+            else 
+            {
+                _timer.Stop();
+                _timerRunning = false;
+                this.StartStop.Text = "Start";
+            }
+        }
+
+        
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            _timer.Stop();
+            _timerRunning = false;
+            SubmitTimeSheet(_currentElapsedTime);
+            _totalElapsedTime = TimeSpan.Zero;
+            _currentElapsedTime = TimeSpan.Zero;
+            _totalElapsedTimeDisplay.Text = _currentElapsedTime.ToString();
+            _currentElapsedTimeDisplay.Text = "00:00:00";
+            _Start = null;
+            this.StartStop.Text = "Start";
+        }
+
+        private void SubmitTimeSheet(TimeSpan totalElapsedTime)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@TotalTime",totalElapsedTime.ToString()),
+                new SqlParameter("@StartTime",_Start),
+                new SqlParameter("@EndTime",DateTime.Now.ToString()),
+            };
+            DataTable table = new DataTable();
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand sqlcmd = new SqlCommand("usp_InsertTimeSheet", connection);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+                foreach (var parameter in parameters)
+                    sqlcmd.Parameters.Add(parameter);
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(sqlcmd);
+                    adapter.Fill(table);
+                }
+                catch (Exception ex)
+                {
+                    //ignore
+                }
+            }
+        }
+
+        private string ConnectionString = $"Data Source=src" +
+                               ";Persist Security Info=True" +
+                               $";Password=password" +
+                               $";User ID=userID" +
+                               $";Initial Catalog=Database" +
+                               $";Connection Timeout=30";
     }
 }
